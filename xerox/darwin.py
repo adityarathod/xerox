fe1193028cea2cb31927fd1c1dbea581ee4712b2
Kenneth Reitz <me@kennethreitz.com>
2010-09-09 00:42:36 -0400

    OSX Support.

---
+++ xerox/darwin.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+""" Copy + Paste in OS X
+"""
+
+
+import subprocess
+import commands
+
+from base import *
+
+
+def copy(string):
+    """Copy given string into system clipboard."""
+    try:
+        subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE).communicate(str(unicode(string)))
+    except Exception, why:
+        raise XcodeNotFound
+    
+    return
+   
+    
+def paste():
+    """Returns system clipboard contents."""
+    try:
+        return unicode(commands.getoutput('pbpaste'))
+    except Exception, why:
+        raise XcodeNotFound
+    
