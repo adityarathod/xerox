b7508232d6fb3c0b304ec54c0cd88f2844becfe5
Kenneth Reitz <me@kennethreitz.com>
2010-09-09 00:42:43 -0400

    Linux support.

---
+++ xerox/linux.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+""" Copy + Paste in Linux
+"""
+
+import subprocess
+from base import *
+
+
+def copy(string):
+    """Copy given string into system clipboard."""
+    try:
+        _cmd = ["xclip", "-selection", "clipboard"]
+        subprocess.Popen(_cmd, stdin=subprocess.PIPE).communicate(str(unicode(string)))
+        return
+    except Exception, why:
+        raise XclipNotFound
+    
+def paste():
+    """Returns system clipboard contents."""
+    try:
+        return unicode(subprocess.Popen(["xclip", "-selection", "clipboard", "-o"], stdout=subprocess.PIPE).communicate()[0])
+    except Exception, why:
+        raise XclipNotFound
+
