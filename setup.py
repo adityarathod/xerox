57ed051bd346e47b7cfb9bbf6f54cc6a42db8608
Kenneth Reitz <me@kennethreitz.com>
2010-09-09 00:43:12 -0400

    Setup.py for pypi.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import xerox
+
+from distutils.core import setup
+
+
+def publish():
+    """Publish to PyPi"""
+    os.system("python setup.py sdist upload")
+
+if sys.argv[-1] == "publish":
+    publish()
+    sys.exit()
+
+setup(name='xerox',
+      version='0.2.0',
+      description='Python wrapper for Gist API',
+      long_description=open('README.rst').read(),
+      author='Kenneth Reitz',
+      author_email='me@kennethreitz.com',
+      url='http://github.com/kennethreitz/xerox.py',
+      packages=['xerox'],
+      license='MIT',
+      classifiers=(
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        )
+      )
