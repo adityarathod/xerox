cd784566e1ce25f11afbf43ca13549afeb548929
Burrito-Bazooka <Burrito-Bazooka@users.noreply.github.com>
2016-06-11 13:15:57 +0100

    Fix detecting piped input
    Fixes case when used inside Python script which is not run from a tty, I bet there are a few other cases it fixes as well.

--- xerox/__init__.py
+++ xerox/__init__.py
@@ -1,12 +1,15 @@
 from .core import *
 
 import sys
+import os
+import stat
 
 def main():
     """ Entry point for cli. """
+    mode = os.fstat(0).st_mode
     if sys.argv[1:]:  # called with input arguments
         copy(' '.join(sys.argv[1:]))
-    elif not sys.stdin.isatty():  # piped in input
+    elif stat.S_ISFIFO(mode) or stat.S_ISREG(mode):  # piped in input
         copy('\n'.join(sys.stdin.readlines()))
     else:  # paste output
         print(paste())
