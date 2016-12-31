bce6e8eba97928efe32c0ff1287fe7b80860754a
Uri Katz <4urikatz@gmail.com>
2016-12-31 13:48:23 +0200

    changed linsep to '\n'

--- xerox/__init__.py
+++ xerox/__init__.py
@@ -1,13 +1,13 @@
 from .core import *
 
 import sys
-import os
+
 
 def main():
     """ Entry point for cli. """
     if sys.argv[1:]:  # called with input arguments
         copy(' '.join(sys.argv[1:]))
     elif not sys.stdin.isatty():  # piped in input
-        copy(''.join(sys.stdin.readlines()).rstrip(os.linesep))
+        copy(''.join(sys.stdin.readlines()).rstrip('\n'))
     else:  # paste output
         print(paste())
