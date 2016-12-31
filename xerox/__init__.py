8a4897fc9cb0192ed91f4e63dbe2da37f4d3ec69
Uri Katz <4urikatz@gmail.com>
2016-12-31 13:28:04 +0200

    Join lines without newline and remove trailing newline

--- xerox/__init__.py
+++ xerox/__init__.py
@@ -1,12 +1,13 @@
 from .core import *
 
 import sys
+import os
 
 def main():
     """ Entry point for cli. """
     if sys.argv[1:]:  # called with input arguments
         copy(' '.join(sys.argv[1:]))
     elif not sys.stdin.isatty():  # piped in input
-        copy('\n'.join(sys.stdin.readlines()))
+        copy(''.join(sys.stdin.readlines()).rstrip(os.linesep))
     else:  # paste output
         print(paste())
